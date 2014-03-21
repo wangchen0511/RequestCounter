0a941e9b0e610fc3a6d7bd9a07029364376b40c8
Chen Wang <wangchen0511@yahoo.com>
2014-03-21 00:19:07 -0700

    thread-based request counter

---
+++ src/main/java/requestCounter/ArrayCircularBuffer.java
@@ -0,0 +1,59 @@
+package requestCounter;
+
+import java.util.Iterator;
+
+public class ArrayCircularBuffer<T> implements ICircularBuffer<T> {
+
+	private final T[] arr;
+	private final int size;
+	private int head = 0;
+	private int tail = -1;
+	private int length = 0;
+	
+	@SuppressWarnings("unchecked")
+	public ArrayCircularBuffer(int size){
+		this.size = size;
+		arr = (T[]) new Object[size];
+	}
+	
+	@Override
+	public void add(T t) {
+		tail = (tail == size - 1) ? 0 : tail + 1;
+		arr[tail] = t;
+		if(length == size){
+			head = (head == size - 1) ? 0 : head + 1;
+		}
+	}
+
+	
+	private class CricularIterator<T> implements Iterator<T>{
+		private int currentPoint = tail;
+		private int currentLength = 0;
+		@Override
+		public boolean hasNext() {
+			if(currentLength == length){
+				return false;
+			}
+			return true;
+		}
+
+		@Override
+		public T next() {
+			@SuppressWarnings("unchecked")
+			T obj = (T) arr[currentPoint];
+			currentPoint = (currentPoint == 0) ? size - 1 : currentPoint - 1;
+			currentLength++;
+			return obj;
+		}
+
+		@Override
+		public void remove() {
+			
+		}}
+	
+	@Override
+	public Iterator<T> iteratorFromTailToHead() {
+		return new CricularIterator<T>();
+	}
+
+}
---
+++ src/main/java/requestCounter/RequestCounterThreadBased.java
@@ -0,0 +1,155 @@
+package requestCounter;
+
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class RequestCounterThreadBased implements IRequestCounter {
+
+	private final AtomicInteger secCounter = new AtomicInteger(0);
+	private final AtomicInteger minCounter = new AtomicInteger(0);
+	private final AtomicLong currentSecReqs = new AtomicLong(0);
+	private final AtomicLong currentMinReqs = new AtomicLong(0);
+	private final AtomicLong currentHourReqs = new AtomicLong(0);
+	private final ICircularBuffer<Long> secQueue = new ArrayCircularBuffer<Long>(60);
+	private final ICircularBuffer<Long> minQueue = new ArrayCircularBuffer<Long>(60);
+	private final ICircularBuffer<Long> hourQueue = new ArrayCircularBuffer<Long>(72);
+	private final ReentrantLock lock = new ReentrantLock();
+	private final ScheduledExecutorService exec = Executors.newScheduledThreadPool(5);
+	
+	private class SecTask implements Runnable{
+
+		@Override
+		public void run() {
+			try{
+				lock.lock();
+				secQueue.add(currentSecReqs.get());
+				currentSecReqs.set(0);
+				if(secCounter.incrementAndGet() == 60){
+					secCounter.set(0);
+					minQueue.add(currentMinReqs.get());
+					currentMinReqs.set(0);
+					if(minCounter.incrementAndGet() == 60){
+						minCounter.set(0);
+						hourQueue.add(currentHourReqs.get());
+						currentHourReqs.set(0);
+					}
+				}
+			}finally{
+				lock.unlock();
+			}	
+		}
+		
+	}
+	
+	public RequestCounterThreadBased(){
+		exec.scheduleAtFixedRate(new SecTask(), 0, 1000, TimeUnit.MILLISECONDS);
+	}
+	
+	
+	
+	@Override
+	public long getLastHour() throws InstantiationException,
+			IllegalAccessException {
+		return getLastSeveralHours(1);
+	}
+
+	@Override
+	public long getThisHour() throws InstantiationException,
+			IllegalAccessException {
+		return currentHourReqs.get();
+	}
+
+	@Override
+	public long getLastMin() throws InstantiationException,
+			IllegalAccessException {
+		return getLastSeveralHours(1);
+	}
+
+	@Override
+	public long getThisMin() throws InstantiationException,
+			IllegalAccessException {
+		return currentMinReqs.get();
+	}
+
+	@Override
+	public long getLastSec() throws InstantiationException,
+			IllegalAccessException {
+		return getLastSeveralSecs(1);
+	}
+
+	@Override
+	public long getThisSec() throws InstantiationException,
+			IllegalAccessException {
+		return currentSecReqs.get();
+	}
+
+	@Override
+	public long getLastSeveralMins(int mins) throws InstantiationException,
+			IllegalAccessException {
+		Iterator<Long> minIter = minQueue.iteratorFromTailToHead();
+		long res = 0;
+		for(int i = 0; i < mins; i++){
+			if(!minIter.hasNext()){
+				break;
+			}
+			res += minIter.next();
+		}
+		return res;
+	}
+	
+	public void addOneRequest(){
+		currentSecReqs.incrementAndGet();
+		currentMinReqs.incrementAndGet();
+		currentHourReqs.incrementAndGet();
+	}
+	
+
+	@Override
+	public long getLastSeveralSecs(int secs) throws InstantiationException,
+			IllegalAccessException {
+		Iterator<Long> secIter = secQueue.iteratorFromTailToHead();
+		long res = 0;
+		for(int i = 0; i < secs; i++){
+			if(!secIter.hasNext()){
+				break;
+			}
+			res += secIter.next();
+		}
+		return res;
+	}
+
+	@Override
+	public long getLastSeveralHours(int hours) throws InstantiationException,
+			IllegalAccessException {
+		Iterator<Long> hourIter = minQueue.iteratorFromTailToHead();
+		long res = 0;
+		for(int i = 0; i < hours; i++){
+			if(!hourIter.hasNext()){
+				break;
+			}
+			res += hourIter.next();
+		}
+		return res;
+	}
+
+	@Override
+	public long getLastDay() throws InstantiationException,
+			IllegalAccessException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getLastSeveralDays(int days) throws InstantiationException,
+			IllegalAccessException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
